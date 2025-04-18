7a6508161e80a22cbb213bca90adcf1809549d05
CT-Dev1 <ryanhrousseau@gmail.com>
2025-04-18 13:36:01 +0100

    added .env key loading

--- weather_functions.py
+++ weather_functions.py
@@ -1,9 +1,13 @@
 import requests
+from dotenv import load_dotenv
+import os
+
 
 def get_weather_data(city):
     '''Retrieves json response from open weather API for specific city. Returns dictionary object'''
+    load_dotenv()
     units = "metric"
-    api_key = "ff4b88bc79dcd581bf3afa2fef167363"
+    api_key = os.getenv("API_KEY")
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={units}"
     response = requests.get(url)
     if response.status_code == 200:
