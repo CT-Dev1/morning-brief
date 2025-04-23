0d34fe23899fa5376ede397933326054e1fe7471
CT-Dev1 <ryanhrousseau@gmail.com>
2025-04-23 12:18:25 +0100

    added documentation

--- src/weather/__init__.py
+++ src/weather/__init__.py
@@ -1,9 +1,5 @@
 # Import functions from weather.py
 from .weather import get_forecast_by_city, get_forecast_by_zip_code
 
-# Load environment variables
-from dotenv import load_dotenv
-load_dotenv()
-
 # Make these functions available on import
 __all__ = ['get_forecast_by_city', 'get_forecast_by_zip_code']
