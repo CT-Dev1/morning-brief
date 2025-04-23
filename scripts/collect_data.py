f49f45802a41e1d6df611ed12db0f75a8dd44a12
CT-Dev1 <ryanhrousseau@gmail.com>
2025-04-23 01:01:39 +0100

    added module initation and loading with sys path work around

---
+++ scripts/collect_data.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+# Add project root directory to Python's path - probably a more elegant way of doing this - enables modules to be found
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Down the line, add a setup.py at the root, restructure everything under src as importable code - see issue #1
+
+from src.weather import get_forecast_by_city, get_forecast_by_zip_code
+
+# collect weather data 
+print(get_forecast_by_city("London", 6))
+
