68d251eca82f04f68f7f8c586293c5639fd98dbc
CT-Dev1 <ryanhrousseau@gmail.com>
2025-04-23 12:59:18 +0100

    added current weather data collection

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import datetime
 
 # Load in .env variables from config (api keys)
 from src.utils.config import API_KEY_OPEN_WEATHER
@@ -79,4 +80,44 @@ def get_forecast_by_zip_code(zip_code :str, country_code: str, num_forecast: int
         forecasted_weather.append(ith_forecast)
     return forecasted_weather
 
+def get_current_weather_city(city: str):
+    """ Get current weather for a city. Returns a dictionary of time, temperature, wind speed, humidity, weather description, cloudiness percentage and sunset time. 
+    Args:
+        city (str): city for weather forecast
+    """
+    # Note future extension to add in error handling for incorrect city names or error status code from API
+    units = 'metric'
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY_OPEN_WEATHER}&units={units}"
+    response = requests.get(url)
+    data = response.json()
+    current_weather = {}
+    current_weather['time'] = datetime.fromtimestamp(data['dt']).strftime('%Y-%m-%d %H:%M:%S') # convert from unix
+    current_weather['temp'] = data['main']['temp']
+    current_weather['wind_speed'] = data['wind']['speed']
+    current_weather['humidity'] = data['main']['humidity']
+    current_weather['weather_desc'] = data['weather'][0]['description']
+    current_weather['cloudiness'] = data['clouds']['all']
+    current_weather['sunset_time'] = datetime.fromtimestamp(data['sys']['sunset']).strftime('%Y-%m-%d %H:%M:%S') #convert from unix
+    return current_weather
+
+def get_current_weather_zipcode(zip_code: str, country_code: str):
+    """ Get current weather for a zip code with country code. Returns a dictionary of time, temperature, wind speed, humidity, weather description, cloudiness percentage and sunset time. 
+    Args:
+        zip_code (str): location zipcode (first part of uk zip, eg. e1 or nw1)
+        country_code (str): country code of zipcode
+    """
+    # Note future extension to add in error handling for incorrect city names or error status code from API
+    units = 'metric'
+    url = f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&appid={API_KEY_OPEN_WEATHER}&units={units}"
+    response = requests.get(url)
+    data = response.json()
+    current_weather = {}
+    current_weather['time'] = datetime.fromtimestamp(data['dt']).strftime('%Y-%m-%d %H:%M:%S') # convert from unix
+    current_weather['temp'] = data['main']['temp']
+    current_weather['wind_speed'] = data['wind']['speed']
+    current_weather['humidity'] = data['main']['humidity']
+    current_weather['weather_desc'] = data['weather'][0]['description']
+    current_weather['cloudiness'] = data['clouds']['all']
+    current_weather['sunset_time'] = datetime.fromtimestamp(data['sys']['sunset']).strftime('%Y-%m-%d %H:%M:%S') #convert from unix
+    return current_weather
 
